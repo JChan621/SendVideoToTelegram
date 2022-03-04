0cab6a6d5c43ef145ee36defdaa15e7d357cca59
fourierz517 <83400846+fourierz517@users.noreply.github.com>
2022-03-03 21:23:32 -0800

    Update sendvideo.py

--- sendvideo.py
+++ sendvideo.py
@@ -2,24 +2,24 @@
 """
 Created on Thu Mar  3 18:12:18 2022
 
-@author: TZEHIM
+@author: Fourier
 """
 
 from telethon.tl.types import DocumentAttributeVideo
 import asyncio
 from telethon import TelegramClient
 import os
-PATH = r'C:\Users\TZEHIM\Documents\GitHub\video-splitter'
-api_id = 1314345
-api_hash = 'b60041bdda5f4ca6a3073c960959a61e'
-bot_token = '1147505946:AAH48sNZfzrf1QBoymNks9tCQs4RHQHj1y4'
-client = TelegramClient('C:/Users/TZEHIM/Downloads/session/', api_id, api_hash)
-client.start(phone = '85222683201',password = 'TakagiReni621')
+PATH = 'your folder dir'
+api_id = 'your api id'
+api_hash = 'your api hash'
+#bot_token = 'your bot token' #Not used
+client = TelegramClient('your telegram session dir', api_id, api_hash)
+client.start(phone = 'your phone',password = 'your password')
 files = os.listdir(PATH)
 print(files)
 async def prog():
     for file in files:
-        if file.startswith('testx'):
+        if file.startswith('testx'): #A list of videos created and named with testx##3.mov.
             await client.send_file('me', os.path.join(PATH, file), attributes = (DocumentAttributeVideo(0, 0, 0),))
 
 with client:
